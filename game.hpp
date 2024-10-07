27c1c856618515bf4e64b2fad0d88bebaed72ef5
R-a-wan <rawanmariamraneem@gmail.com>
2024-10-07 10:34:09 -0700

    Create game.hpp

---
+++ game.hpp
@@ -0,0 +1,13 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+int checkWinner(char board[3][3]);
+
+int play(int x);
+void dis_board();
+bool checkfull();
+void menu();
+void dis_history();
+void c_history();
+
+#endif
